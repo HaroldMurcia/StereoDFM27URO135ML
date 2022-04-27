88435c8d74efd92993bcf772452a7e7066efea28
sirui1 <2420171062@estudiantesunibague.edu.co>
2022-04-27 15:44:15 -0500

    ac

--- include/utility.h
+++ include/utility.h
@@ -26,7 +26,7 @@ class ParamServer{
 public:
     
     YAML::Node node = YAML::LoadFile("src/Stereo_DFM_27URO135_ML/config/params.yaml");
-    YAML::Node stereo_dfm_27uro135_ml = node["stereo_dfm_27uro135_ml"];
+    YAML::Node stereo_dfm_27uro135_ml = node["stereo_dfm27uro135ml"];
 
     YAML::Node properties = stereo_dfm_27uro135_ml["properties"];
     YAML::Node formats = stereo_dfm_27uro135_ml["formats"];
